e95cebf771c0194d7ff7e33075a2859a86d41562
benbasty <benbasty@yahoo.fr>
2023-03-08 10:52:13 +0800

    current class pathname

--- src/Header.js
+++ src/Header.js
@@ -1,66 +1,29 @@
-import React from "react";
+import {React} from "react";
 import ShowCase from "./ShowCase";
 import {Link, useLocation } from "react-router-dom";
 
 function Header() {
     const location = useLocation();
-    if (location.pathname === '/miraclehotel') {
-        return (
-            <div>
-                <header>          
-                    <nav id="navbar">
-                        <div className="container">
-                            <h1 className="logo"><Link to="/">MIRACLE</Link></h1>
-                                <ul>
-                                    <li><Link className="current" to="/miraclehotel">Home</Link></li>
-                                    <li><Link to="miraclehotel/about">About</Link></li>
-                                    <li><Link to="miraclehotel/contact">Contact</Link></li>
-                                </ul> 
-                        </div>
-                    </nav>
-                    <ShowCase />                
-                </header>
-            </div>
-        );
-    }
-    if (location.pathname === '/about') {
-        return (
-            <div>
-                <header>          
-                    <nav id="navbar">
-                        <div className="container">
-                            <h1 className="logo"><Link to="/">MIRACLE</Link></h1>
-                                <ul>
-                                    <li><Link to="/">Home</Link></li>
-                                    <li><Link className="current" to="/about">About</Link></li>
-                                    <li><Link to="/contact">Contact</Link></li>
-                                </ul> 
-                        </div>
-                    </nav>
-                    <ShowCase />                
-                </header>
-            </div>
-        );
-    }
-    if (location.pathname === '/contact') {
-        return (
-            <div>
-                <header>          
-                    <nav id="navbar">
-                        <div className="container">
-                            <h1 className="logo"><Link to="/">MIRACLE</Link></h1>
-                                <ul>
-                                    <li><Link to="/">Home</Link></li>
-                                    <li><Link to="/about">About</Link></li>
-                                    <li><Link className="current" to="/contact">Contact</Link></li>
-                                </ul> 
-                        </div>
-                    </nav>
-                    <ShowCase />                
-                </header>
-            </div>
-        );
-    }
-    
+    const homeClass = location.pathname === "/miraclehotel" ? "current" : "";
+    const aboutClass = location.pathname === "/miraclehotel/about" ? "current" : "";
+    const contactClass = location.pathname === "/miraclehotel/contact" ? "current" : "";
+
+    return (
+        <div>
+            <header>
+                <nav id="navbar">
+                    <div className="container">
+                        <h1 className="logo"><Link to="/">MIRACLE</Link></h1>
+                            <ul>
+                                <li><Link to="/miraclehotel" className={homeClass}>Home</Link></li>
+                                <li><Link to="miraclehotel/about" className={aboutClass}>About</Link></li>
+                                <li><Link to="miraclehotel/contact" className={contactClass}>Contact</Link></li>
+                            </ul>
+                    </div>
+                </nav>
+                <ShowCase />
+            </header>
+        </div>
+    );
 }
 export default Header;
